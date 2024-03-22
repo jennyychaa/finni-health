45d0e343a69042b4247731a1577eee1ef9a5b559
JC <16579792+jennyychaa@users.noreply.github.com>
2024-03-21 22:04:57 -0400

    Added layout templates

---
+++ frontend/src/templates/WideLayout.tsx
@@ -0,0 +1,7 @@
+import { PropsWithChildren } from 'react';
+
+const WideLayout = ({ children }: PropsWithChildren) => {
+  return <div className='container mx-auto px-8 py-24'>{children}</div>;
+};
+
+export default WideLayout;
